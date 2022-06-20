645a8377de1c400d7589daa13aae269738985a48
kritipandeyrupc <kritipandeyrupc@gmail.com>
2022-06-20 21:18:38 +0530

    my second commit

--- src/secondtest/LeapYearTest.java
+++ src/secondtest/LeapYearTest.java
@@ -3,45 +3,69 @@ package secondtest;
 public class LeapYearTest {
 
 	public static void main(String[] args) {
-	
-	
+
+	}
+
+	public static boolean checkLeapYearOrNot(String date) {
+		if (checkDatepattern(date)) {
+			int year = getYear(date);
+			if (year % 4 == 0) {
+				if (year % 100 == 0) {
+					if (year % 400 == 0) {
+						return true;
+					} else {
+						return false;
+					}
+
+				} else {
+					return true;
+				}
+
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+
 	}
 
 	public static boolean checkDatepattern(String date) {
-		if(date.matches("^((0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))-((0[1-9])|(1[1-2]))-([0-9]{4})$")) {
+		if (date.matches("^((0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))-((0[1-9])|(1[1-2]))-([0-9]{4})$")) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
 
 	public static int getDate(String date) {
-		String []dateArray = date.split("-");
-		return Integer.parseInt(dateArray[dateArray.length-3]);
-		
+		String[] dateArray = date.split("-");
+		return Integer.parseInt(dateArray[dateArray.length - 3]);
+
 	}
 
 	public static int getMonth(String date) {
-		String []dateArray = date.split("-");
-		return Integer.parseInt(dateArray[dateArray.length-2]);
-		
+		String[] dateArray = date.split("-");
+		return Integer.parseInt(dateArray[dateArray.length - 2]);
+
 	}
 
 	public static int getYear(String date) {
-		String []dateArray = date.split("-");
-		return Integer.parseInt(dateArray[dateArray.length-1]);
-		
+		String[] dateArray = date.split("-");
+		return Integer.parseInt(dateArray[dateArray.length - 1]);
+
 	}
 
 	private static boolean checkDivisibleByHundred(String year) {
-		String Value = year.substring(2,4);
+		String Value = year.substring(2, 4);
 		int lastTwoNumber = Integer.parseInt(Value);
-		if(lastTwoNumber == 0) {
+		if (lastTwoNumber == 0) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
-		
+
 	}
 
 }
+
